be931cb4d5ff63233208092f4f9a72af87ef2154
LetThereBeLemons <jamsieh@icloud.com>
2022-08-01 13:01:08 +0100

    fixed

--- release/install.sh
+++ release/install.sh
@@ -7,11 +7,13 @@ read -p "Choice: " choice
 if [ $choice -eq 1 ]; then
 	echo "Cloning..."
 	mkdir cahp-master
-	git clone https://github.com/ltblem/cahp/tree/master cahp-master
+	git clone https://github.com/ltblem/cahp cahp-master
 	echo "The latest release is now in the directory cahp-master. Use config.nim to configure, then run build.sh to build."
 elif [ $choice -eq 2 ]; then
-	echo "Downloading..."
-	mkdir cahp-4
-	git clone https://github.com/ltblem/cahp/tree/4 cahp-4
-	echo "The latest release is now in the directory cahp-4. Use config.nim to configure, then run build.sh to build."
+	echo "This doesn't work at the moment."
+	#TODO: Make this work
+	#//echo "Downloading..."
+	#//mkdir cahp-4
+	#//git clone https://github.com/ltblem/cahp/tree/4 cahp-4
+	#//echo "The latest release is now in the directory cahp-4. Use config.nim to configure, then run build.sh to build."
 fi
