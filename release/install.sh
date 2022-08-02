ace1e0b6a48e6376456dd8fd75c0011640e19d1c
LetThereBeLemons <jamsieh@icloud.com>
2022-08-02 15:07:07 +0100

    I'm not that good at shell scripting.

--- release/install.sh
+++ release/install.sh
@@ -2,17 +2,17 @@
 echo "Download https://github.com/ltblem/cahp to directory $HOME/cahp-master?"
 read -p "[Y/n] " choice
 
-if [ $choice = "y" ]; then
+if [ $choice == "y" ]; then
 	echo "Cloning..."
 	mkdir cahp-master
 	git clone https://github.com/ltblem/cahp $HOME/cahp-master
 	echo "The latest release is now in the directory cahp-master. Use config.nim to configure, then run build.sh to build."
-elif [ $choice = "Y" ]; then
+elif [ $choice == "Y" ]; then
 	echo "Cloning..."
 	mkdir cahp-master
 	git clone https://github.com/ltblem/cahp $HOME/cahp-master
 	echo "The latest release is now in the directory cahp-master. Use config.nim to configure, then run build.sh to build."
-elif [ $choice = "" ]; then
+elif [ $choice == "" ]; then
 	echo "Cloning..."
 	mkdir cahp-master
 	git clone https://github.com/ltblem/cahp $HOME/cahp-master
